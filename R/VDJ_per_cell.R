5c439e9ebf4ea1f98eede95112b20a39b5f99bd6
rodamian <43376200+rodamian@users.noreply.github.com>
2020-12-16 13:57:33 +0100

    Add files via upload

--- R/VDJ_per_cell.R
+++ R/VDJ_per_cell.R
@@ -1,6 +1,6 @@
 #' Analyzes and processes the repertoire sequencing data from cellranger vdj. This provides information on the single-cell level for each clone, as opposed to the output from VDJ_analyze.
 #' @title VDJ_per_cell
-#' @param VDJ.out.directory Character vector with each element containing the path to the output of cellranger vdj runs. This corresponds to the same object used for the VDJ_analyze function. Multiple repertoires to be integrated in a single transcriptome should be supplied as multiple elements of the character vector. This can be left blank if supplying the clonotypes and contig files directly as input. This pipeline assumes that the output file names have not been changed from the default 10x settings in the /outs/ folder. This is compatible with B and T cell repertoires (both separately and simultaneously).
+#' @param VDJ.directory Character vector with each element containing the path to the output of cellranger vdj runs. This corresponds to the same object used for the VDJ_analyze function. Multiple repertoires to be integrated in a single transcriptome should be supplied as multiple elements of the character vector. This can be left blank if supplying the clonotypes and contig files directly as input. This pipeline assumes that the output file names have not been changed from the default 10x settings in the /outs/ folder. This is compatible with B and T cell repertoires (both separately and simultaneously).
 #' @param clonotype.list Output from either VDJ_analyze or VDJ_clonotype functions. This list should correspond to a single GEX.list object, in which each list element in clonotype.list is found in the GEX.object. Furthermore, the i'th entry in the directory supplied to GEX.list should correspond to the i'th element in the clonotype.list object.
 #' @param contig.list List of dataframe based on the all_contigs.csv file from cellranger vdj output. If 10x sequencing was not used then this object should be formatted with the same columns as the 10x object.
 #' @param fasta.list Contains the full-length sequence information in the same format as filtered_contig.fasta file from the output of cellranger.
@@ -10,10 +10,10 @@
 #' @export
 #' @examples
 #' \dontrun{
-#' check_VDJ_per_cell <- VDJ_per_cell(clonotype.list = output.from.VDJ_analyze, VDJ.out.directory = "path/to/cellranger/outs/")
+#' check_VDJ_per_cell <- VDJ_per_cell(clonotype.list = output.from.VDJ_analyze, VDJ.directory = "path/to/cellranger/outs/")
 #' }
 VDJ_per_cell <- function(clonotype.list,
-                         VDJ.out.directory,
+                         VDJ.directory,
                          contig.list,
                          fasta.list,
                          reference.list,
@@ -26,34 +26,39 @@ VDJ_per_cell <- function(clonotype.list,
   require(stringr)
   require(Biostrings)
   library(parallel)
-  if(.Platform$OS.type == "unix") options(mc.cores=detectCores(), print("Using parallel package")) else options(mc.cores=1)
-  if(missing(VDJ.out.directory)) print("No output directory supplied. Assuming clonotype and contig are provided as list objects")
+  if(.Platform$OS.type == "unix") options(mc.cores=detectCores() -1 , print("Using parallel package")) else options(mc.cores=1)
+  if(missing(VDJ.directory)) print("No output directory supplied. Assuming clonotype and contig are provided as list objects")
   if(missing(contig.list)) print("No contig.list supplied. Assuming contigs should be extracted from working directory")
   if(missing(filtered.contigs)) filtered.contigs <- TRUE
 
-  # Accounting for directory name with or without "/"
-  if (substr(VDJ.out.directory, nchar(VDJ.out.directory), nchar(VDJ.out.directory)) == "/") {slash <- ""} else {slash <- "/"}
-  mouse_number <- substr(sub('.*mouse', '', VDJ.out.directory), 1, 1)
+  # Extracting mouse_number from dir name
+  if (all(str_detect(VDJ.directory, "mouse"))) {mouse_number <- substr(sub('.*mouse', '', VDJ.directory), 1, 1)}
 
   print("Reading in input files")
 
-  if(missing(VDJ.out.directory)==F) {
-    if(filtered.contigs==T) VDJ.out.directory_contigs <- paste0(VDJ.out.directory, slash, "filtered_contig_annotations.csv")
-    if(filtered.contigs==F) VDJ.out.directory_contigs <- paste0(VDJ.out.directory, slash, "all_contig_annotations.csv")
-    VDJ.out.directory_fasta <- paste0(VDJ.out.directory, slash, "filtered_contig.fasta")
-    VDJ.out.directory_reference <- paste0(VDJ.out.directory, slash, "concat_ref.fasta")
-    VDJ.out.directory_annotations <- paste0(VDJ.out.directory, slash, "all_contig_annotations.json")
+  # Reading files -----------------------------------------------------------
 
-    contig.list <- lapply(VDJ.out.directory_contigs, function(x) utils::read.csv(x, stringsAsFactors = FALSE,sep=",",header=T))
-    fasta.list <- lapply(VDJ.out.directory_fasta, function(x) seqinr::read.fasta(x, as.string = T,seqonly = F,forceDNAtolower = F))
-    reference.list <- lapply(VDJ.out.directory_reference, function(x) seqinr::read.fasta(x, as.string = T,seqonly = F,forceDNAtolower = F))
-    annotations.list <- lapply(VDJ.out.directory_annotations, read_json)
+  if(missing(VDJ.directory)==F) {
+    if (str_sub(VDJ.directory, -1) == "/") {slash <- ""} else {slash = "/"}
+    if(filtered.contigs==T) VDJ.directory_contigs <- paste0(VDJ.directory, slash, "/filtered_contig_annotations.csv")
+    if(filtered.contigs==F) VDJ.directory_contigs <- paste0(VDJ.directory, slash, "/all_contig_annotations.csv")
+    VDJ.directory_fasta <- paste0(VDJ.directory, slash, "/filtered_contig.fasta")
+    VDJ.directory_reference <- paste0(VDJ.directory, slash, "/concat_ref.fasta")
+    VDJ.directory_annotations <- paste0(VDJ.directory, slash, "/all_contig_annotations.json")
+
+    contig.list <- lapply(VDJ.directory_contigs, function(x) utils::read.csv(x, stringsAsFactors = FALSE,sep=",",header=T))
+    fasta.list <- lapply(VDJ.directory_fasta, function(x) seqinr::read.fasta(x, as.string = T,seqonly = F,forceDNAtolower = F))
+    reference.list <- lapply(VDJ.directory_reference, function(x) seqinr::read.fasta(x, as.string = T,seqonly = F,forceDNAtolower = F))
+    annotations.list <- lapply(VDJ.directory_annotations, read_json)
   }
 
   VDJ.per.cell <- list()
 
-  ### each element in VDJ.per.cell is a dataframe containing per cell information
+  # each element in VDJ.per.cell is a dataframe containing per cell  --------
+
   for(i in 1:length(clonotype.list)) {
+
+  # Reference and barcodes --------------------------------------------------
 
     contig.list[[i]]$chain[which(contig.list[[i]]$chain == "TRB")] <- "IGH"
     contig.list[[i]]$chain[which(contig.list[[i]]$chain == "TRA")] <- "IGK"
@@ -63,7 +68,8 @@ VDJ_per_cell <- function(clonotype.list,
     for (x in clonotype.list[[i]]$clonotype_id) {
       temp_index <- gsub("_consensus_", "_concat_ref_",
                          contig.list[[i]]$raw_consensus_id[contig.list[[i]]$raw_clonotype_id == x
-                                                           & contig.list[[i]]$chain == "IGH" & contig.list[[i]]$is_cell == "True"])
+                       & contig.list[[i]]$chain == "IGH" & contig.list[[i]]$is_cell == "True"])
+
       references_HC[[x]] <- as.character(reference.list[[i]][unique(temp_index[temp_index != "None"])])
       if (length(references_HC[[x]]) == 0) {references_HC[[x]] <- ""}
     }
@@ -82,6 +88,9 @@ VDJ_per_cell <- function(clonotype.list,
     for (x in clonotype.list[[i]]$clonotype_id) {
       barcode[[x]] <- contig.list[[i]]$barcode[contig.list[[i]]$raw_clonotype_id==x]
     }
+
+
+  # Trimmed sequences -------------------------------------------------------
 
     # Selecting only sequences with start and end annotations
     filtered <- mclapply(annotations.list[[i]], function(x)
@@ -104,39 +113,47 @@ VDJ_per_cell <- function(clonotype.list,
                  sequence <- sapply(clono, function(x) x$sequence)
                  contig_id <-  sapply(clono, function(x) x$contig_name)
                  chain <- sapply(clono, function(x) x$annotations[[1]]$feature$chain)
-                 data.frame(contig_id = contig_id, chain = chain, sequence = substr(sequence, temp_start, temp_end))
+                 data.frame(contig_id = contig_id, chain = chain, sequence = substr(sequence, temp_start + 1, temp_end))
                }
       )
     }
 
     seq_trimmed <- extract_trimmed(seq_per_clonotype(annotations.list[[i]], barcode))
 
-    # Aligns trimmed sequences against reference to get trimmed reference sequence
-    get_alinged_ref <- function(trimmed, reference, chain) {
-      if (reference == "") {return("")} else {
+
+  # Aligns trimmed sequences against reference to get trimmed reference --------
+
+    get_aligned_ref <- function(trimmed, reference, chain) {
+      if (length(reference) > 1) {reference = reference[which.min(str_length(reference))]}
+      if (reference == "") {return("")}
+      else {
         alignments <- pairwiseAlignment(trimmed$sequence[trimmed$chain == chain], reference, type = "local")
         as.character(subject(alignments[which.max(score(alignments))]))
       }
     }
 
-    ref_trimmed_HC <- mcmapply(get_alinged_ref, seq_trimmed, references_HC, MoreArgs = list("IGH"))
-    ref_trimmed_LC <- mcmapply(get_alinged_ref, seq_trimmed, references_LC, MoreArgs = list("IGK"))
+    print("Alignment of trimmed sequences to get trimmed germline. This will take ~2 minutes")
+    ref_trimmed_HC <- mcmapply(get_aligned_ref, seq_trimmed, references_HC, MoreArgs = list("IGH"))
+    ref_trimmed_LC <- mcmapply(get_aligned_ref, seq_trimmed, references_LC, MoreArgs = list("IGK"))
+
+
+  # Pasted cdr3 -------------------------------------------------------------
 
     pasted_cdr3 <- mclapply(barcode, function(x) {
 
-      match_HC <- contig.list[[i]]$barcode %in% x & contig.list[[i]]$is_cell=="True" & contig.list[[i]]$chain=="IGH"
-      match_LC <- contig.list[[i]]$barcode %in% x & contig.list[[i]]$is_cell=="True" & contig.list[[i]]$chain=="IGK"
-      if (sum(match_HC) > sum(match_LC))
-        {temp_barcode <- contig.list[[i]]$barcode[match_HC]}
-      else
-        {temp_barcode <- contig.list[[i]]$barcode[match_LC]}
+      cdr3_HC <- filter(contig.list[[i]][contig.list[[i]]$barcode %in% x,],
+                        is_cell=="True", productive=="True", chain=="IGH") %>% select (cdr3, barcode, chain)
+      cdr3_LC <- filter(contig.list[[i]][contig.list[[i]]$barcode %in% x,],
+                        is_cell=="True", productive=="True", chain %in% c("IGK", "IGL")) %>% select (cdr3, barcode, chain)
 
-     data.frame(pasted_cdr3 = paste0(contig.list[[i]]$chain[match_HC], ":", contig.list[[i]]$cdr3[match_HC], ";",
-                                contig.list[[i]]$chain[match_LC], ":", contig.list[[i]]$cdr3[match_LC]),
-                barcode = temp_barcode)
+      cdr3 <- inner_join(cdr3_HC, cdr3_LC, by="barcode", suffix = c("_HC", "_LC"))
+
+       data.frame(pasted_cdr3 = paste0(cdr3$chain_HC, ":", cdr3$cdr3_HC, ";", cdr3$chain_LC, ":",cdr3$cdr3_LC),
+                        CDR3H = cdr3$cdr3_HC, CDR3L = cdr3$cdr3_LC, barcode = cdr3$barcode)
     })
 
-    # Extracting gene usage features from contig.list
+  # Extracting gene usage features from contig.list -------------------------
+
     extract_feature <- function(barcodes, trimmed_sequence, trimmed_reference, feature, chain) {
       matching <- contig.list[[i]]$barcode%in%barcodes & contig.list[[i]]$chain == chain & contig.list[[i]]$is_cell == "True"
       if (trimmed_reference == "") {trimmed_reference <- rep("", length(contig.list[[i]]$barcode[matching]))}
@@ -156,9 +173,10 @@ VDJ_per_cell <- function(clonotype.list,
     VDJ.per.cell[[i]] <- mcMap(inner_join, data_HC, data_LC, by="barcode", suffix=list(c("_HC", "_LC")))
     VDJ.per.cell[[i]] <- mcMap(left_join, VDJ.per.cell[[i]], pasted_cdr3, by="barcode", suffix=list(c("", "")))
 
+  # Adding mouse number -------------------------
     for (j in 1:length(VDJ.per.cell[[i]])) {
       if (nrow(VDJ.per.cell[[i]][[j]]) != 0)
-        VDJ.per.cell[[i]][[j]]$mouse <- mouse_number
+        VDJ.per.cell[[i]][[j]]$mouse <- mouse_number[[i]]
     }
 
   }
