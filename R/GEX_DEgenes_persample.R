2694c9d2297f23870bc94b94a68d7f13c865130f
Kabooma7 <63246605+Kabooma7@users.noreply.github.com>
2021-01-31 10:38:28 +0100

    Update GEX_DEgenes_persample.R
    Adding the option for a heatmap

--- R/GEX_DEgenes_persample.R
+++ R/GEX_DEgenes_persample.R
@@ -6,14 +6,23 @@
 #' @param sample2 either character or integer specifying the first sample that should be compared.
 #' @param by.group Logical specifying if groups should be used instead of samples. If TRUE, then the argument in sample1 and sample2 will correspond to cells found in the groups from sample1 or sample2.
 #' @param filter Character vector of initials of the genes to be filtered. Default is c("MT-", "RPL", "RPS"), which filters mitochondrial and ribosomal genes.
+#' @param return.plot Logical specifying if a heatmap of the DEX genes is to be returned. If TRUE then @return is a list where the first element is a dataframe and the second a heatmap (see @return) 
+#' @param logFC Logical specifying whether the genes will be displayed based on logFC (TRUE) or pvalue (FALSE).
+#' @param up.genes Integer specifying the number of upregulated genes to be shown.
+#' @param down.genes Integer specifying the number of downregulated genes to be shown. 
 #' @return Returns a dataframe containing the output from the FindMarkers function, which contains information regarding the genes that are differentially regulated, statistics (p value and log fold change), and the percent of cells expressing the particular gene for both groups.
 #' @export
 #' @examples
 #' \dontrun{
 #' check_de_genes2 <- GEX_DEgenes_persample(automate.GEX=automate.GEX.output[[i]],min.pct = .25,sample1 = "2",sample2 = "3")
 #'}
-GEX_DEgenes_persample <- function(automate.GEX, min.pct, sample1, sample2, by.group, filter){
+GEX_DEgenes_persample <- function(automate.GEX, min.pct, sample1, sample2, by.group, filter, return.plot, logFC, up.genes, down.genes){
 
+    if(missing(return.plot)) return.plot <- FALSE  
+    if(missing(logFC)) logFC <- TRUE 
+    if(missing(up.genes)) up.genes <- 15 
+    if(missing(down.genes)) down.genes <- 15 
+    
     if(missing(by.group)) by.group <- FALSE
     if (missing(filter)) filter <- c("MT-", "RPL", "RPS")
 
@@ -23,11 +32,27 @@ GEX_DEgenes_persample <- function(automate.GEX, min.pct, sample1, sample2, by.gr
 
     cluster_markers <- Seurat::FindMarkers(automate.GEX, min.pct = min.pct, ident.1 = as.character(sample1), ident.2 = as.character(sample2))
     cluster_markers$SYMBOL <- rownames(cluster_markers)
+    
     exclude <- c()
     for (j in filter) {
         exclude <- c(exclude, !rownames(cluster_markers) %in% j)
     }
     cluster_markers <- cluster_markers[-exclude,]
-
-    return(cluster_markers)
+    
+    if (return.plot==TRUE) {
+      if (logFC==TRUE) {
+      ranks <- order(-cluster_markers$avg_logFC)
+      cluster_markers <- cluster_markers[ranks,]
+      heatmap_genes <- c(cluster_markers$SYMBOL[1:up.genes], cluster_markers$SYMBOL[(nrow(cluster_markers)-down.genes):nrow(cluster_markers)])
+      }
+      if (logFC==FALSE) {
+      ranks <- order(cluster_markers$p_val_adj)
+      cluster_markers <- cluster_markers[ranks,]
+      heatmap_genes <- c(cluster_markers[which(cluster_markers$avg_logFC > 0),"SYMBOL"][1:up.genes], cluster_markers[which(cluster_markers$avg_logFC < 0),"SYMBOL"][1:down.genes])
+      }
+      cluster_markers_heatmap <- DoHeatmap(automate.GEX, features = heatmap_genes)
+    }
+    if (return.plot==FALSE) cluster_markers_heatmap <- NULL
+    
+    return(list(cluster_markers, cluster_markers_heatmap))
 }
