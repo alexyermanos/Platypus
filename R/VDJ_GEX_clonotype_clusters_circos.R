82aa43e26ba6b68a359339ed806c3d7a95e378dd
raphkuhn <76953491+raphkuhn@users.noreply.github.com>
2021-04-19 02:22:02 +0200

    Updated Circos functions to work with new VDJ.GEX.matrix format
    switch between ond and new version: Version="new" -> VDJ_GEX_matrix format, Version="old" (or anything else) -> Old file format

--- R/VDJ_GEX_clonotype_clusters_circos.R
+++ R/VDJ_GEX_clonotype_clusters_circos.R
@@ -1,5 +1,5 @@
 #'Makes a Circos plot from the VDJ_GEX_integrate output. Connects the clonotypes with the corresponding clusters.
-#' @param vdj.gex.integrate.output The output of the VDJ_GEX_integrate function. A list of data frames for each sample containing the clonotype information and cluster membership information.
+#' @param VDJ.GEX.matrix The output of the VDJ_GEX_integrate function. A list of data frames for each sample containing the clonotype information and cluster membership information.
 #' @param TopX Plots only the top X most expanded clonotypes. By default all clonotypes are shown.
 #' @param label.threshold Minimal amount of clonotypes per gene neccessary to add a gene label to the sector. Default: 0.
 #' @param c.count Show clonotype or cell count on Circos plot. Default = T.
@@ -9,58 +9,148 @@
 #'  plots <- VDJ_clonotype_clusters_circos(vdj_gex_integrate_test, topX=100, label.threshold=5)
 #'}
 
-VDJ_clonotype_clusters_circos <- function(vdj.gex.integrate.output, topX, label.threshold, axis, c.count, n_cluster){
+VDJ_clonotype_clusters_circos <- function(VDJ.GEX.matrix, topX, label.threshold, axis, c.count, n_cluster, version){
   if(missing(topX)){topX <- "all"}
   if(missing(n_cluster)){print("Please specify cluster number n_cluster")}
   if(missing(label.threshold)){label.threshold <- 1}
-  if(topX != "all"){
-    for(k in 1:length(vdj.gex.integrate.output)){
-      vdj.gex.integrate.output[[k]] <- head(vdj.gex.integrate.output[[k]], topX)
-    }
-  }
   if(missing(axis)){axis <- "max"}
   if(missing(c.count)){c.count <-T}
+  if(missing(version)){version <- "new"}
 
-  adj.matrix <- list()
-  clonotypes <- c()
-  for (k in 1:length(vdj.gex.integrate.output)){
-    print(k)
-    adj.matrix[[k]] <- matrix(nrow =nrow(vdj.gex.integrate.output[[k]]), ncol = n_cluster)
-    for (i in 1:nrow(vdj.gex.integrate.output[[k]])){
-      for (j in 1:n_cluster){
-        adj.matrix[[k]][i,j]<-as.numeric(str_split(vdj.gex.integrate.output[[k]]$cluster_membership_percent, pattern = ",")[[i]])[j]
-        adj.matrix[[k]][i,j] <- adj.matrix[[k]][i,j]*length(str_split(vdj.gex.integrate.output[[k]]$cell_index[[i]], pattern=";")[[1]])/100 #Put here #cell_index -> has to be splitted by ; to get length()
-        rownames(adj.matrix[[k]]) <- vdj.gex.integrate.output[[k]]$clonotype_id
-        clonotypes <- append(clonotypes, vdj.gex.integrate.output[[k]]$clonotype_id, after= length(clonotypes))
-        colnames(adj.matrix[[k]]) <- paste("cluster", 0:(ncol(adj.matrix[[k]])-1), sep = " ") #c("cluster 0", "cluster 1","cluster 2","cluster 3","cluster 4","cluster 5","cluster 6","cluster 7","cluster 8","cluster 9","cluster 10")
+  
+  if(version == "new"){
+      print("Reminder: VDJ_VJ_usage_circos() funcion built for new Platypus X.X.X is being used. Output of VDJ_GEX_matrix() required as input.")
+      adj.matrix <- list()
+      clonotypes <- c()
+      
+      #fill empty entries
+      VDJ.GEX.matrix[[1]]$clonotype_id_10x[which(VDJ.GEX.matrix[[1]]$clonotype_id_10x == "")] <- "None"
+      
+      #split VDJ.GEX.matrix into samples
+      VDJ.GEX_list <- list()
+      for (i in 1:length(table(VDJ.GEX.matrix[[1]]$sample_id))){
+        VDJ.GEX_list[[i]] <- VDJ.GEX.matrix[[1]][which(VDJ.GEX.matrix[[1]]$sample_id==paste0("s",i)),]
+      }
+      
+      # MAKE Clonotype df
+      clonotypes <- list()
+      if(topX != "all"){
+        for(k in 1:length(VDJ.GEX_list)){
+          clonotypes[[k]] <- names(head(sort(table(VDJ.GEX_list[[1]]$clonotype_id_10x),decreasing = T),topX))
         }
+      }else{
+        for (k in 1:length(VDJ.GEX_list)){
+          topX <- length(table(VDJ.GEX_list[[k]]$clonotype_id_10x))
+          clonotypes[[k]] <- names(head(sort(table(VDJ.GEX_list[[1]]$clonotype_id_10x),decreasing = T),topX))
+        }
+      }
+      #filter and keep only cells of topX clonotypes
+      for (k in 1:length(VDJ.GEX_list)){
+        VDJ.GEX_list[[k]] <- VDJ.GEX_list[[k]][which(VDJ.GEX_list[[k]]$clonotype_id_10x %in% clonotypes[[k]]),]
+      }
+      clonotypes_all <- NA
+      
+      for (k in 1:length(VDJ.GEX_list)){
+        print(paste0("Processing Sample ", k))
+        n_cluster <- length(table(VDJ.GEX_list[[k]][which(VDJ.GEX_list[[k]]$clonotype_id_10x %in% clonotypes[[k]]),"seurat_clusters"]))
+        adj.matrix[[k]] <- matrix(nrow =length(clonotypes[[k]]), ncol = n_cluster)
+        rownames(adj.matrix[[k]]) <- clonotypes[[k]]
+        colnames(adj.matrix[[k]]) <- paste("cluster", 0:(ncol(adj.matrix[[k]])-1), sep = " ")
+        clonotypes_all <- append(clonotypes_all, clonotypes[[k]], after= length(clonotypes_all))
+        
+        for (i in 1:nrow(adj.matrix[[k]])){
+
+           adj.matrix[[k]][i,] <- table(VDJ.GEX_list[[k]][which(VDJ.GEX_list[[k]]$clonotype_id_10x==rownames(adj.matrix[[k]])[i]),"seurat_clusters"])
+           
+           # adj.matrix[[k]][i,j]<-as.numeric(str_split(VDJ.GEX_list[[k]]$cluster_membership_percent, pattern = ",")[[i]])[j]
+            # adj.matrix[[k]][i,j] <- adj.matrix[[k]][i,j]*length(str_split(VDJ.GEX_list[[k]]$cell_index[[i]], pattern=";")[[1]])/100 #Put here #cell_index -> has to be splitted by ; to get length()
+            # rownames(adj.matrix[[k]]) <- VDJ.GEX_list[[k]]$clonotype_id
+            # clonotypes <- append(clonotypes, VDJ.GEX_list[[k]]$clonotype_id, after= length(clonotypes))
+            # colnames(adj.matrix[[k]]) <- paste("cluster", 0:(ncol(adj.matrix[[k]])-1), sep = " ") #c("cluster 0", "cluster 1","cluster 2","cluster 3","cluster 4","cluster 5","cluster 6","cluster 7","cluster 8","cluster 9","cluster 10")
+        }
+        adj.matrix[[k]][is.nan(adj.matrix[[k]])] = 0
+      }
+      
+      
+      print("Set colours")
+    
+      ggplotColours <- function(n = 6, h = c(0, 360) + 15){
+        if ((diff(h) %% 360) < 1) h[2] <- h[2] - 360/n
+        hcl(h = (seq(h[1], h[2], length = n)), c = 100, l = 65)
+      }
+      cluster_col <- ggplotColours(n=n_cluster)
+    
+      clonotypes_all <- unique(clonotypes_all)
+      cluster_col <- setNames(cluster_col,colnames(adj.matrix[[1]]))
+      clonotypes_col <- setNames(rainbow(length(clonotypes_all)),sample(clonotypes_all))
+      grid.col <- append(cluster_col, clonotypes_col)
+    
+      print("Plotting")
+      plot <- list()
+      for (i in 1:length(VDJ.GEX_list)){
+        print(i)
+        print(length(VDJ.GEX_list))
+        print(adj.matrix[[i]])
+        nm = unique(unlist(dimnames(adj.matrix[[i]])))
+        group = structure(gsub('[[:digit:]]+', '', nm), names = nm)
+        group = factor(group[sample(length(group), length(group))], levels = c("cluster ", "clonotype"))
+        plot[[i]] <- VDJ_circos(adj.matrix[[i]], group = group, grid.col = grid.col, label.threshold = label.threshold, axis = axis, c.count=c.count)
+      }
+      
+      plot[[i+1]] <- adj.matrix
+      
+  }else{
+    ########################
+    print("Reminder: VDJ_VJ_usage_circos() funcion built for Platypus 2.0.5 is being used. Output of VDJ_analyze() required as input. Set [version = new] for compatibility with VDJ_GEX_matrix().")
+    adj.matrix <- list()
+    clonotypes <- c()
+    
+    if(topX != "all"){
+      for(k in 1:length(VDJ.GEX.matrix)){
+        VDJ.GEX.matrix[[k]] <- head(VDJ.GEX.matrix[[k]], topX)
+      }
     }
-    adj.matrix[[k]][is.nan(adj.matrix[[k]])] = 0
+    for (k in 1:length(VDJ.GEX.matrix)){
+      print(k)
+      n_cluster <-  length(str_split(VDJ.GEX.matrix[[k]]$cluster_membership_percent, pattern = ",")[[1]])
+      adj.matrix[[k]] <- matrix(nrow =nrow(VDJ.GEX.matrix[[k]]), ncol = n_cluster)
+      for (i in 1:nrow(VDJ.GEX.matrix[[k]])){
+        for (j in 1:n_cluster){
+          adj.matrix[[k]][i,j]<-as.numeric(str_split(VDJ.GEX.matrix[[k]]$cluster_membership_percent, pattern = ",")[[i]])[j]
+          adj.matrix[[k]][i,j] <- adj.matrix[[k]][i,j]*length(str_split(VDJ.GEX.matrix[[k]]$cell_index[[i]], pattern=";")[[1]])/100 #Put here #cell_index -> has to be splitted by ; to get length()
+          rownames(adj.matrix[[k]]) <- VDJ.GEX.matrix[[k]]$clonotype_id
+          clonotypes <- append(clonotypes, VDJ.GEX.matrix[[k]]$clonotype_id, after= length(clonotypes))
+          colnames(adj.matrix[[k]]) <- paste("cluster", 0:(ncol(adj.matrix[[k]])-1), sep = " ") #c("cluster 0", "cluster 1","cluster 2","cluster 3","cluster 4","cluster 5","cluster 6","cluster 7","cluster 8","cluster 9","cluster 10")
+        }
+      }
+      adj.matrix[[k]][is.nan(adj.matrix[[k]])] = 0
+    }
+    print("Set colours")
+    
+    ggplotColours <- function(n = 6, h = c(0, 360) + 15){
+      if ((diff(h) %% 360) < 1) h[2] <- h[2] - 360/n
+      hcl(h = (seq(h[1], h[2], length = n)), c = 100, l = 65)
+    }
+    cluster_col <- ggplotColours(n=n_cluster)
+    
+    clonotypes <- unique(clonotypes)
+    cluster_col <- setNames(cluster_col,colnames(adj.matrix[[1]]))
+    clonotypes_col <- setNames(rainbow(length(clonotypes)),sample(clonotypes))
+    grid.col <- append(cluster_col, clonotypes_col)
+    
+    print("Plotting")
+    plot <- list()
+    for (i in 1:length(VDJ.GEX.matrix)){
+      print(i)
+      print(length(VDJ.GEX.matrix))
+      print(adj.matrix[[i]])
+      nm = unique(unlist(dimnames(adj.matrix[[i]])))
+      group = structure(gsub('[[:digit:]]+', '', nm), names = nm)
+      group = factor(group[sample(length(group), length(group))], levels = c("cluster ", "clonotype"))
+      plot[[i]] <- VDJ_circos(adj.matrix[[i]], group = group, grid.col = grid.col, label.threshold = label.threshold, axis = axis, c.count=c.count)
+    }
+    plot[[i+1]] <- adj.matrix
   }
-  print("Set colours")
-
-  ggplotColours <- function(n = 6, h = c(0, 360) + 15){
-    if ((diff(h) %% 360) < 1) h[2] <- h[2] - 360/n
-    hcl(h = (seq(h[1], h[2], length = n)), c = 100, l = 65)
-  }
-  cluster_col <- ggplotColours(n=n_cluster)
-
-  clonotypes <- unique(clonotypes)
-  cluster_col <- setNames(cluster_col,colnames(adj.matrix[[1]]))
-  clonotypes_col <- setNames(rainbow(length(clonotypes)),sample(clonotypes))
-  grid.col <- append(cluster_col, clonotypes_col)
-
-  print("Plotting")
-  plot <- list()
-  for (i in 1:length(vdj.gex.integrate.output)){
-    print(i)
-    print(length(vdj.gex.integrate.output))
-    print(adj.matrix[[i]])
-    nm = unique(unlist(dimnames(adj.matrix[[i]])))
-    group = structure(gsub('[[:digit:]]+', '', nm), names = nm)
-    group = factor(group[sample(length(group), length(group))], levels = c("cluster ", "clonotype"))
-    plot[[i]] <- VDJ_circos(adj.matrix[[i]], group = group, grid.col = grid.col, label.threshold = label.threshold, axis = axis, c.count=c.count)
-  }
-  plot[[i+1]] <- adj.matrix
+  
   return(plot)
 }
