9a3aa4722a3a499120197f40e97e365dfca69f64
danielleshlesinger <82576835+danielleshlesinger@users.noreply.github.com>
2021-07-02 09:55:00 +0200

    more fixes

--- R/GEX_volcano.R
+++ R/GEX_volcano.R
@@ -1,9 +1,10 @@
 #' Plots a volcano plot from the output of the FindMarkers function from the Seurat package or the GEX_cluster_genes function alternatively.
 #' @param findmarkers.output output data frame from the FindMarkers function from the Seurat package or GEX_cluster_genes list output.
-#' @param cluster.genes.output logical specifying wheter GEX_cluster_genes output is being used.
-#' @param condition.1 either character or integer specifying ident.1 that was used in the FindMarkers function from the Seurat package. Should be left empty when using the GEX_cluster_genes output.
-#' @param condition.2 either character or integer specifying ident.2 that was used in the FindMarkers function from the Seurat package. Should be left empty when using the GEX_cluster_genes output.
+#' @param cluster.genes.output logical specifying whether GEX_cluster_genes output is being used.
+#' @param condition.1 either character or integer specifying ident.1 that was used in the FindMarkers function from the Seurat package. Will be used for Plot title. Should be left empty when using the GEX_cluster_genes output.
+#' @param condition.2 either character or integer specifying ident.2 that was used in the FindMarkers function from the Seurat package. Will be used for Plot title. Should be left empty when using the GEX_cluster_genes output.
 #' @param explicit.title logical specifying whether the title should include logFC information for each condition.
+#' @param MT.Rb.filter Logical, should Mitotic and Ribosomal genes be filtered out of the geneset. True by default.
 #' @param color.p.threshold numeric specifying the adjusted p-value threshold for geom_points to be colored. Default is set to 0.01.
 #' @param color.log.threshold numeric specifying the absolute logFC threshold for geom_points to be colored. Default is set to 0.25.
 #' @param label.p.threshold numeric specifying the adjusted p-value threshold for genes to be labeled via geom_text_repel. Default is set to 0.001.
@@ -13,24 +14,51 @@
 #' @param by.logFC logical. If set to TRUE n.label.up and n.label.down will label genes ordered by logFC instead of adjusted p-value. 
 #' @param maximum.overlaps integer specifying removal of labels with too many overlaps. Default is set to Inf.
 #' @param plot.adj.pvalue logical specifying whether adjusted p-value should by plotted on the y-axis. 
-#' @return Returns a volcano plot from the output of the FindMarkers function from the Seurat package, which is a ggplot object that can be modified or plotted. Infinite p-values are set defined value of the highest -log(p) + 100.
+#' @param platypus.version Function works with V2 and V3, no need to set this parameter
+#' @return Returns a volcano plot from the output of the FindMarkers function from the Seurat package, or from the GEX_cluster_genes Platypus function alternatively. 
+#' Output is a ggplot object that can be modified or plotted. Infinite p-values are set defined value of the highest -log(p) + 100.
 #' @export
 #' @examples
 #' \dontrun{
 #' #using the findmarkers.output
 #' GEX_volcano_1 <- GEX_volcano(findmarkers.output = FindMarkers.Output, condition.1 = "cluster1", condition.2 = "cluster2", maximum.overlaps = 20)
 #' GEX_volcano_2 <- GEX_volcano(findmarkers.output = FindMarkers.Output, condition.1 = "cluster1", condition.2 = "cluster2", n.label.up = 50, n.label.down = 20)
+#' 
 #' #using the GEX_cluster_genes output
 #' GEX_volcano_3 <- GEX_volcano(findmarkers.output = GEX_cluster_genes.Output, cluster.genes.output =T)
 #'}
-GEX_volcano <- function(findmarkers.output, cluster.genes.output, condition.1, condition.2, explicit.title, RP.MT.filter, color.p.threshold, color.log.threshold, label.p.threshold, label.logfc.threshold, n.label.up, n.label.down, by.logFC ,maximum.overlaps ,plot.adj.pvalue) {
-  require(ggrepel)
+GEX_volcano <- function(findmarkers.output, 
+                        cluster.genes.output, 
+                        condition.1, 
+                        condition.2, 
+                        explicit.title, 
+                        RP.MT.filter, 
+                        color.p.threshold, 
+                        color.log.threshold, 
+                        label.p.threshold, 
+                        label.logfc.threshold, 
+                        n.label.up, n.label.down, 
+                        by.logFC,
+                        maximum.overlaps,
+                        plot.adj.pvalue, 
+                        platypus.version){
   
-  if(missing(cluster.genes.output)){cluster.genes.output <- F}
+  if(missing(cluster.genes.output) & missing(findmarkers.output)){
+    stop("Please provide either FindMarkers or GEX_cluster_genes input for this function")}
+  if(missing(cluster.genes.output)){
+    cluster.genes.output <- F
+    print("Using FindMarkers as input")}
+  else{
+      print("Using GEX_cluster_genes as input")}
   if(missing(condition.1)){condition.1 <- ""}
   if(missing(condition.2)){condition.2 <- ""}
-  if(missing(explicit.title)){explicit.title <- T}
-  if(missing(RP.MT.filter)){RP.MT.filter <- T}
+  if(condition.1 =="" &  condition.2 == "" & cluster.genes.output == F){print("Conditions not provided and will not be displayed in plot title")}
+  if(missing(explicit.title)){
+    explicit.title <- T
+    print("explicit.title parameter set to T")}
+  if(missing(RP.MT.filter)){
+    RP.MT.filter <- T
+    print("RP.MT.filter parameter set to T")}
   if(missing(color.p.threshold)){color.p.threshold <- 0.01}
   if(missing(color.log.threshold)){color.log.threshold <- 0.25}
   if(missing(label.p.threshold)){label.p.threshold <- 0.001}
@@ -38,8 +66,19 @@ GEX_volcano <- function(findmarkers.output, cluster.genes.output, condition.1, c
   if(missing(n.label.up)){n.label.up <- F}
   if(missing(n.label.down)){n.label.down <- F}
   if(missing(by.logFC)){by.logFC <- F}
-  if(missing(maximum.overlaps)) {maximum.overlaps <- Inf}
-  if(missing(plot.adj.pvalue)){plot.adj.pvalue <- F}
+  if(missing(maximum.overlaps)){
+    maximum.overlaps <- Inf
+    print("maximum.overlaps parameter was set to Inf")}
+  if(missing(plot.adj.pvalue)){
+    plot.adj.pvalue <- F
+    print("plot.adj.pvalue parameter was set to F")}
+  
+  platypus.version <- "Does not matter"
+  
+  if(n.label.up == F & n.label.down == F){
+    print(paste0("Geom_points colored in red represent genes with adjusted p-value < ", color.p.threshold, " and logFC > ", color.log.threshold))
+    print(paste0("Geom_points that are labeled represent genes with adjusted p-value < ", label.p.threshold, " and logFC > ", label.logfc.threshold))
+  }
   
   if(class(n.label.up) != class(n.label.down)){
     temp <- list(n.label.up, n.label.down)
@@ -48,6 +87,15 @@ GEX_volcano <- function(findmarkers.output, cluster.genes.output, condition.1, c
     n.label.down <- n.genes
     print("Same number of up- and downregulated genes will be labeled")
   } # Assuming the same number of up- and downregulated genes are to be labeled if only one is specified
+  
+  if(class(n.label.up) == "numeric" & class(n.label.down) == "numeric"){
+    print(paste0("Geom_points colored in red represent genes with adjusted p-value < ", color.p.threshold, " and logFC > ", color.log.threshold))
+    print(paste0("Top ", n.label.up, " and bottom ",n.label.down, " genes will be labeled"))
+  }
+  
+  genes <- NULL
+  minus.log10p <- NULL
+  minus.log10p_adj <- NULL
   
   if(cluster.genes.output == F) {
     
@@ -222,3 +270,4 @@ GEX_volcano <- function(findmarkers.output, cluster.genes.output, condition.1, c
   }
   return(output.plot)
 }
+
